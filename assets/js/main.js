06ada0d4b85cb960f45195286cd3d22fb94752cb
freetimezp <kraftukrnet@ukr.net>
2023-09-09 21:37:20 +0300

    08. js-portfolio-28-museum. Collection section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,33 @@ window.addEventListener('load', () => {
 });
 
 
+//swiper 
+var swiper = new Swiper('.artsSwiper', {
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {
+            shadow: false,
+            translate: [0, 0, -400]
+        },
+        next: {
+            translate: ['100%', 0, 0]
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

a0afdc519de2452af5dc097c433c42865c2ea0b8
freetimezp <kraftukrnet@ukr.net>
2023-09-10 22:22:55 +0300

    12. js-portfolio-28-museum. Responsive.

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,7 +189,13 @@ window.addEventListener('load', () => {
     }
 });
 
-
+//toggle menu 
+const toggleMenu = () => {
+    const menu = document.querySelector('.menu');
+    const navMobile = document.querySelector('.nav-mobile');
+    menu.classList.toggle('active');
+    navMobile.classList.toggle('active');
+}
 
 
 
